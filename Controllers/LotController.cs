3c54aa5fa916be5e943f526eb9f95743af12d797
Nahuel <nahuelavila670@gmail.com>
2023-11-16 20:36:26 -0300

    Controllers project

---
+++ Controllers/LotController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace Controllers
+{
+
+    public class LotController
+    {
+        private LotModel model;
+
+        public LotController()
+        {
+            LotModel m = new LotModel();
+            model = m;
+        }
+
+        public List<LotModel> GetAll(int id_camion)
+        {
+            return model.GetAll(id_camion);
+        }
+
+        public bool MarkAsDelivered(int id, int id_almacen)
+        {
+            return model.MarkAsDelivered(id, id_almacen);
+        }
+
+        public bool UnmarkAsDelivered(int id, int id_almacen)
+        {
+            return model.UnmarkAsDelivered(id, id_almacen);
+        }
+    }
+
+}
---
+++ Controllers/TruckController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Models;
+
+namespace Controllers.TruckController
+{
+    public class TruckController
+    {
+        private TruckModel model;
+
+        public TruckController()
+        {
+            TruckModel whModel = new TruckModel();
+            model = whModel;
+        }
+
+        public List<TruckModel> GetTruckByUserId(int id_usuario)
+        {
+            return model.GetTruckByUserId(id_usuario);
+        }
+    }
+}
